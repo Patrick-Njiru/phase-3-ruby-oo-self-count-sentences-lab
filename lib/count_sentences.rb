2b61c59b3659246aead7973e3caac2389cae4f3a
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-24 12:43:35 +0300

    Completed assignment.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
-require 'pry'
+# require 'pry'
 
 class String
+  attr_accessor :string
+
+  def initialize(string)
+    @string = string
+  end
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split.filter {|sub_string| sub_string.end_with?(".", "?", "!") ? sub_string : nil }.length
   end
 end
